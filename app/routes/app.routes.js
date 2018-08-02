9ecb465444fb8cef4097a7e6ea369f398d19e3f6
wcho <wcho0907@gmail.com>
2018-08-02 10:23:19 +0800

    Save loader init

--- app/routes/app.routes.js
+++ app/routes/app.routes.js
@@ -1,5 +1,9 @@
 module.exports = (app) => {
     const ranking = require('../controllers/ranking.controller.js');
 
+    const loader = require('../controllers/loader.controller.js');
+
     app.get('/ranking', ranking.rankAll);
+
+    app.get('/loader', loader.loadAll)
 }

259ad709bea613d952e37896d398d8de4367e749
wcho <wcho0907@gmail.com>
2018-08-29 17:31:21 +0800

    Remove default value in schema and init token API

--- app/controllers/token.controller.js
+++ app/controllers/token.controller.js
@@ -1,3 +1,6 @@
+const async = require('async');
+var request = require('request');
+
 exports.handleAll = (req, res) => {
     res.send({"test": "test"});
 }
--- doc/db-schema.sql
+++ doc/db-schema.sql
@@ -3,11 +3,11 @@ DROP TABLE IF EXISTS `ct_udf_history`;
 
 CREATE TABLE `ct_udf_history` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `resolution` varchar(3) NOT NULL DEFAULT '',
-  `exchange` varchar(30) NOT NULL DEFAULT '',
+  `resolution` varchar(3) NOT NULL,
+  `exchange` varchar(30) NOT NULL,
   `startUnixTimestampSec` bigint(15) unsigned NOT NULL,
-  `baseTokenSymbol` varchar(32) CHARACTER SET latin1 NOT NULL DEFAULT '',
-  `quoteTokenSymbol` varchar(32) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `baseTokenSymbol` varchar(32) CHARACTER SET latin1 NOT NULL,
+  `quoteTokenSymbol` varchar(32) CHARACTER SET latin1 NOT NULL,
   `openPrice` decimal(30,18) unsigned NOT NULL,
   `highPrice` decimal(30,18) unsigned NOT NULL,
   `lowPrice` decimal(30,18) unsigned NOT NULL,

95924f953291e397ee68b07cfb7ab7b9958fbf47
wcho <wcho0907@gmail.com>
2018-08-30 12:05:02 +0800

    Write aggregator alogorithm

--- app/controllers/aggregator.controller.js
+++ app/controllers/aggregator.controller.js
@@ -5,17 +5,24 @@ exports.aggregateTo = (req, res) => {
     var exchange = req.query.exchange;
     var base = req.query.base;
     var quote = req.query.quote;
-    var timeFrom = req.query.timeFrom;
     var resolutionTo = req.query.resolutionTo;
+    var timeFrom = req.query.timeFrom;
+    var timeTo = req.query.timeTo;
 
-    console.log(exchange + "-" + base + "-" + quote + "-" + timeFrom + "-" + resolutionTo);
+    console.log(exchange + "-" + base + "-" + quote + "-" + resolutionTo + "-" + timeFrom + "-" + timeTo);
 
-    // (Optipon)timeFrom 以timeFrom開始計算, 若是此range(1m)無資料, 則停止
-    // 1. 找此 resolution 下一筆資料之起始時間, 若：
-    //      a. 無資料: 找1m第一個此resolution的起始時間
-    //         prev = 1330518155 - (1330518155 % 1800);
-    //         next = $prev + 1800;
-    // 2. 
+    // S0. 如果有timeFrom則以 timeFrom 或 0 為起始時間,接續S2
+    // S1. 找此resolution(以下 此res)之起始時間(最大的ts):
+    //   S1.1 有資料, 則起始時間=ts+此res(sec),接續S2
+    //   S1.2 沒資料, 停止. (為資料初始情況I, 必須設定起始時間timeFrom為0,或1m有資料之時間)
+    // S2. 校正起始時間至正確刻度
+    //      adjSTime = 1330518155 - (1330518155 % 300); // ex. 300(5m)
+    // S3. 取得時間區間內1m資料
+    //   S3.1 有資料, 則計算出此res的OHLCV
+    //   S3.2 無資料, 停止.
+    // S4. 寫入此res之OHLCV資料
+    // S5. 如果有timeTo檢查(>=)是否停止
+    // S6. 起始時間=本次起始時間+此res(sec),接續S3
 
     res.send({"test": "aggregateTo"});
 }
